db3cbbaa9638e64772ff072d67cf11315362246c
jhylacey <jhylacey@proton.me>
2023-03-12 16:48:17 -0500

    Add Etch a Sketch functionality

--- script.js
+++ script.js
@@ -1,64 +1,45 @@
-/* 
-
-1. using javascript,  create a 16x16 grid of square divs
- -create an html container to house all the divs
- -append the divs using DOM
- -make them appear as a box using flexbox
- -make the maximum length 960px
-
-2. apply event listeners to each square for entering and
-exiting the div
-
-3. on entering exiting "hovering" change the color of the square
-
-4. make a new grid using user input for x and y sides 
-with 100 being the maximum for each side
-
-5. remove old grid
-
-6. make each square change to a random color that increases its
-saturation by 10 with each pass
-
-
-*/
-
-let userLength;
-let userHeight;
-userLength;
-
-drawLength = () => {
-
-}
-
-drawHeight = () => {
-
-}
-
 
 drawGrid = () => {
-    const numRows = 10;
-    const numColumns = 30;
+    const numRows = prompt("choose the number of rows", "");
+    const numColumns = prompt('choose the number of columns', "")
     const container = document.querySelector("#container");
 
-    for (let i = 0; i <= numRows; i++) {
+    for (let i = 1; i <= numRows; i++) {
         const rowDiv = document.createElement("div");
-        rowDiv.textContent = "row";
+        rowDiv.className = 'squareRow';
+        // rowDiv.textContent = "ROW";
         container.appendChild(rowDiv);
 
-        for (let j = 0; j <= numColumns; j++) {
+        for (let j = 1; j <= numColumns; j++) {
             const columnDiv = document.createElement("div");
-            columnDiv.textContent = "column";
+
+            columnDiv.className = "squareColumn";
+            // columnDiv.textContent = "column"
             rowDiv.appendChild(columnDiv);
+            columnDiv.addEventListener('mouseover', function () {
+                columnDiv.className = "squareColumnHover"
+            })
         }
     }
 
 }
 
+
+
+drawGrid()
+
+
 heightButton = document.querySelector('#heightSubmit');
 heightButton.addEventListener('click', drawHeight)
 
 lengthButton = document.querySelector('#lengthSubmit');
-lengthButton.addEventListener('click', drawLength)
+lengthButton.addEventListener('click', function () {
+    const squareList = (document.querySelectorAll(".squareColumnHover"));
+    for (let i = 0; i < squareList.length; i++) {
+        squareList[i].className = "squareColumn"
+    }
+}
+)
 
 
 // getUserInput()
